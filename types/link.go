b49bf8710d5af6a62eeebd5ea96942fb64cb2990
Leon Lux <leonlux@icloud.com>
2024-12-09 17:04:43 +0100

    add unmarshal to link.go

--- types/link.go
+++ types/link.go
@@ -1,8 +1,21 @@
 package types
 
+import (
+	"fmt"
+)
+
 type Link struct {
 	NodeFrom      string
 	NodeTo        string
 	InterfaceFrom string
 	InterfaceTo   string
+}
+
+func (l Link) MarshalYAML() (interface{}, error) {
+	return map[string]interface{}{
+		"endpoints": []string{
+			fmt.Sprintf("%s:%s", l.NodeFrom, l.InterfaceFrom),
+			fmt.Sprintf("%s:%s", l.NodeTo, l.InterfaceTo),
+		},
+	}, nil
 }
