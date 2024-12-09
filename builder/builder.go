299ec1189a4fea3d9f209e0066fa6b90bcf63b38
Leon Lux <leonlux@icloud.com>
2024-12-09 17:05:42 +0100

    add interface for topology builders

---
+++ builder/builder.go
@@ -0,0 +1,16 @@
+package builder
+
+import (
+	"github.com/Lachstec/digsinet-ng/types"
+)
+
+// Builder represents types that can deploy a types.Topology to a network emulation testbed.
+// The concrete semantics are defined by the specifics of the used network emulator.
+type Builder interface {
+	// DeployTopology deploys the specified types.Topology to the network emulator.
+	// Returns an error if something goes wrong while deploying the types.Topology.
+	DeployTopology(types.Topology) error
+	// DestroyTopology destroy the specified Topology, leaving no data remaining.
+	// Returns an error if the given types.Topology is not valid or if something goes wrong while destruction.
+	DestroyTopology(types.Topology) error
+}
