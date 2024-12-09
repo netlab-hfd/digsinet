7203d953124f9aa74f7b7e1be61e3f0819e3d4c8
Leon Lux <leonlux@icloud.com>
2024-12-09 17:05:27 +0100

    add simple test for topology

---
+++ types/topology_test.go
@@ -0,0 +1,29 @@
+package types
+
+import (
+	_ "embed"
+	"fmt"
+	"github.com/go-yaml/yaml"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+//go:embed testdata/simple_topo.yaml
+var expectedTopology string
+
+func TestTopology(t *testing.T) {
+	builder := NewTopologyBuilder()
+	builder.Name("srlceos01")
+	builder.AddNode("nokia_srlinux", "srlinux")
+	builder.AddNode("arista_ceos", "ceos")
+	builder.AddLink("nokia_srlinux", "arista_ceos", "e1-1", "eth1")
+
+	topo := builder.Build()
+	output, err := yaml.Marshal(topo)
+	if err != nil {
+		t.Fatal(fmt.Sprintf("Failed to marshal topology: %s", err))
+	}
+
+	t.Log(string(output))
+	assert.Equal(t, expectedTopology, string(output), "Topology is not as expected")
+}
