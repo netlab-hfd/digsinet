37252b2bebf038f4e55e68172baaffa81bbd3941
Leon Lux <leonlux@icloud.com>
2025-01-10 22:15:57 +0100

    add logging with zerolog to rest server startup

--- server/rest_server.go
+++ server/rest_server.go
@@ -3,7 +3,7 @@ package server
 // adapted from https://github.com/vsouza/go-gin-boilerplate
 import (
 	"github.com/Lachstec/digsinet-ng/config"
-	"log"
+	"github.com/rs/zerolog/log"
 )
 
 func InitRESTServer() {
@@ -11,7 +11,9 @@ func InitRESTServer() {
 	r := NewRESTRouter()
 	err := r.Run(conf.GetString("server.port"))
 	if err != nil {
-		log.Fatal("Unable to start REST server.")
+		log.Fatal().
+			Err(err).
+			Msg("Failed to start REST server")
 		return
 	}
 }
