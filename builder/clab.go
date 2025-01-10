fc66384423e5ad163ec790f31f72187a11139ef2
Leon Lux <leonlux@icloud.com>
2025-01-10 21:50:22 +0100

    add logging via zerolog to clab builder

--- builder/clab.go
+++ builder/clab.go
@@ -4,8 +4,8 @@ import (
 	"bytes"
 	"fmt"
 	"github.com/Lachstec/digsinet-ng/types"
+	"github.com/rs/zerolog/log"
 	"gopkg.in/yaml.v3"
-	"log"
 	"os/exec"
 )
 
@@ -17,7 +17,10 @@ func NewClabBuilder() *ClabBuilder {
 }
 
 func (b *ClabBuilder) DeployTopology(topology types.Topology) error {
-	log.Print("Deploying Topology with Containerlab Builder...")
+
+	log.Info().
+		Str("Builder", b.Id()).
+		Msg("Deploying Topology...")
 
 	// Prepare the command
 	proc := exec.Command("clab", "deploy", "--topo", "-")
@@ -25,6 +28,9 @@ func (b *ClabBuilder) DeployTopology(topology types.Topology) error {
 	// Connect stdin for the process
 	stdin, err := proc.StdinPipe()
 	if err != nil {
+		log.Error().
+			Str("Builder", b.Id()).
+			Msg("Failed to create stdin pipe")
 		return fmt.Errorf("failed to get stdin pipe: %w", err)
 	}
 
@@ -38,35 +44,53 @@ func (b *ClabBuilder) DeployTopology(topology types.Topology) error {
 
 	// Start the process
 	if err = proc.Start(); err != nil {
+		log.Error().
+			Str("Builder", b.Id()).
+			Msg("Failed to create stdin pipe")
 		return fmt.Errorf("failed to start process: %w", err)
 	}
 
 	// Serialize the topology spec
 	topologySpec, err := yaml.Marshal(topology)
 	if err != nil {
+		log.Error().
+			Str("Builder", b.Id()).
+			Msg("Failed to marshal topology spec")
 		return fmt.Errorf("failed to marshal topology: %w", err)
 	}
 
-	// Debug: print the topology spec to ensure correctness
-	log.Printf("Topology spec to be sent: %s", string(topologySpec))
+	log.Debug().
+		Str("Builder", b.Id()).
+		Msg("Topology Spec to be sent: ")
 
 	// Write the topology spec to stdin
 	_, err = stdin.Write(topologySpec)
 	if err != nil {
+		log.Error().
+			Str("Builder", b.Id()).
+			Msg("Failed to write topology spec to stdin")
 		return fmt.Errorf("failed to write to stdin: %w", err)
 	}
 
 	// Close stdin to signal the end of input
 	if err := stdin.Close(); err != nil {
+		log.Error().
+			Str("Builder", b.Id()).
+			Msg("Failed to close stdin")
 		return fmt.Errorf("failed to close stdin: %w", err)
 	}
 
 	// Wait for the process to complete
 	if err = proc.Wait(); err != nil {
+		log.Error().
+			Str("Builder", b.Id()).
+			Msg("Failed to deploy topology")
 		return fmt.Errorf("process finished with error: %w stdout: %s stderr: %s", err, stdout, stderr)
 	}
 
-	log.Print("Topology deployment completed successfully.")
+	log.Info().
+		Str("Builder", b.Id()).
+		Msg("Successfully deployed topology")
 
 	// Start interfaces of the nodes
 	//log.Print("Starting interfaces of the nodes...")
@@ -86,7 +110,9 @@ func (b *ClabBuilder) DeployTopology(topology types.Topology) error {
 }
 
 func (b *ClabBuilder) DestroyTopology(topology types.Topology) error {
-	log.Print("Destroying Topology with Containerlab Builder...")
+	log.Info().
+		Str("Builder", b.Id()).
+		Msg("Destroying Topology...")
 
 	// Prepare the command
 	proc := exec.Command("clab", "destroy", "--topo", "-")
@@ -94,6 +120,9 @@ func (b *ClabBuilder) DestroyTopology(topology types.Topology) error {
 	// Connect stdin for the process
 	stdin, err := proc.StdinPipe()
 	if err != nil {
+		log.Error().
+			Str("Builder", b.Id()).
+			Msg("Failed to create stdin pipe")
 		return fmt.Errorf("failed to get stdin pipe: %w", err)
 	}
 
@@ -107,34 +136,57 @@ func (b *ClabBuilder) DestroyTopology(topology types.Topology) error {
 
 	// Start the process
 	if err = proc.Start(); err != nil {
+		log.Error().
+			Str("Builder", b.Id()).
+			Msg("Failed to start process")
 		return fmt.Errorf("failed to start process: %w", err)
 	}
 
 	// Serialize the topology spec
 	topologySpec, err := yaml.Marshal(topology)
 	if err != nil {
+		log.Error().
+			Str("Builder", b.Id()).
+			Msg("Failed to marshal topology spec")
 		return fmt.Errorf("failed to marshal topology: %w", err)
 	}
 
 	// Debug: print the topology spec to ensure correctness
-	log.Printf("Topology spec to be sent: %s", string(topologySpec))
+	log.Debug().
+		Str("Builder", b.Id()).
+		Msg("Topology Spec to be sent: ")
 
 	// Write the topology spec to stdin
 	_, err = stdin.Write(topologySpec)
 	if err != nil {
+		log.Error().
+			Str("Builder", b.Id()).
+			Msg("Failed to write topology spec to stdin")
 		return fmt.Errorf("failed to write to stdin: %w", err)
 	}
 
 	// Close stdin to signal the end of input
 	if err := stdin.Close(); err != nil {
+		log.Error().
+			Str("Builder", b.Id()).
+			Msg("Failed to close stdin")
 		return fmt.Errorf("failed to close stdin: %w", err)
 	}
 
 	// Wait for the process to complete
 	if err = proc.Wait(); err != nil {
+		log.Error().
+			Str("Builder", b.Id()).
+			Msg("Failed to deploy topology")
 		return fmt.Errorf("process finished with error: %w stdout: %s stderr: %s", err, stdout, stderr)
 	}
 
-	log.Print("Topology successfully destroyed.")
+	log.Info().
+		Str("Builder", b.Id()).
+		Msg("Successfully deployed topology")
 	return nil
+}
+
+func (b *ClabBuilder) Id() string {
+	return "Containerlab"
 }
